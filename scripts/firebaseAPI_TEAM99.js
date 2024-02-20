fbd82bb5804a00f9c57721953ac0bd0aa0fdb96b
YEALI KIM <YKIM229@MY.BCIT.CA>
2024-02-20 10:41:29 -0800

    Finish demo 07

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyB0Sw76jzDiRc3SlvC-2isQ67WPDvBdPG4",
+    authDomain: "comp1800-202410-demo-cb84f.firebaseapp.com",
+    projectId: "comp1800-202410-demo-cb84f",
+    storageBucket: "comp1800-202410-demo-cb84f.appspot.com",
+    messagingSenderId: "839127438086",
+    appId: "1:839127438086:web:b7d37ee5ee4882b14fc487"
 };
 
 //--------------------------------------------
